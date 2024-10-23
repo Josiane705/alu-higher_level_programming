d907111011aaa9ada0dfdb64110206379b21ca7b
Josiane705 <j.mukeshima@alustudent.com>
2024-10-23 00:46:29 -0700

    8-uppercase.py

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+# Author - Josiane Mukeshimana
+
+
+def uppercase(str):
+    """Function to print a string in uppercase."""
+    result = ""
+    for char in str:
+        if ord(char) >= 97 and ord(char) <= 122:
+            result += chr(ord(char) - 32)
+        else:
+            result += char
+    print("{}".format(result))
