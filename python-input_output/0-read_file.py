55ea0dfec00cd19b9e2a56d1384edc85efe9a866
Josiane705 <j.mukeshima@alustudent.com>
2024-11-14 11:17:53 -0800

    0-read_file.py

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,5 +1,17 @@
 #!/usr/bin/python3
-read_file = __import__('0-read_file').read_file
 
+"""
+This module contains a function to read a text file and print its contents to stdout.
+"""
 
-read_file("my_file_0.txt")
+def read_file(filename=""):
+    """
+    Reads a text file and prints its content to stdout.
+
+    Args:
+        filename (str): The name of the file to read. Defaults to an empty string,
+                         meaning no file will be opened unless a valid filename is provided.
+    """
+    with open(filename, 'r', encoding='utf-8') as file:
+        print(file.read(), end="")
+
