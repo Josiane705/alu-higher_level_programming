9b6897651ac82e373b847710c7040b82c8e9fc30
Josiane705 <j.mukeshima@alustudent.com>
2024-11-14 12:40:49 -0800

    12-pascal_triangle.py

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -1,23 +1,19 @@
 #!/usr/bin/python3
-"""This module defines a class Student"""
+"""This module defines a Pascal's Triangle function"""
 
 
-class Student:
-    """Represent a student."""
+def pascal_triangle(n):
+    """Represents Pascal's Triangle of size n
+    """
+    if n <= 0:
+        return []
 
-    def __init__(self, first_name, last_name, age):
-        self.first_name = first_name
-        self.last_name = last_name
-        self.age = age
-
-    def to_json(self, attrs=None):
-        if (type(attrs) == list and
-                all(type(ele) == str for ele in attrs)):
-            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
-        return self.__dict__
-
-    def reload_from_json(self, json):
-        """Replaces all attributes of the Student
-        """
-        for k, v in json.items():
-            setattr(self, k, v)
+    triangles = [[1]]
+    while len(triangles) != n:
+        t = triangles[-1]
+        tmp = [1]
+        for i in range(len(t) - 1):
+            tmp.append(t[i] + t[i + 1])
+        tmp.append(1)
+        triangles.append(tmp)
+    return triangles
