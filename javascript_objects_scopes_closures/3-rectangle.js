68c1f22bda86a5f1d5b8d8487cdf7e73872d4002
Josiane705 <j.mukeshima@alustudent.com>
2025-02-04 01:54:48 -0800

    3-rectangle.js

--- javascript_objects_scopes_closures/3-rectangle.js
+++ javascript_objects_scopes_closures/3-rectangle.js
@@ -1,19 +1,21 @@
 #!/usr/bin/node
 const Rectangle = class Rectangle {
-  constructor(w, h) {
+  constructor (w, h) {
     if (w > 0 && h > 0) {
       this.width = w;
       this.height = h;
     }
   }
 
-  print() {
-    const row = 'X'.repeat(this.width);
-    for (let i = 0; i < this.height; i++) {
-      console.log(row);
+  print () {
+    let prints = '';
+    for (let xx = 0; xx < this.height; xx++) {
+      for (let yy = 0; yy < this.width; yy++) {
+        prints = prints + 'X';
+      }
+      console.log(prints);
+      prints = '';
     }
   }
 };
-
 module.exports = Rectangle;
-
